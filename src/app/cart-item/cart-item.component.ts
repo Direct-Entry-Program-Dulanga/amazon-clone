a78ae253a1b85e7e78a3c160d1f08d01b607c4dc
Dulanga Nimesha <idulanganimesha@gmail.com>
2021-10-30 21:39:05 +0530

    update -cart-item- template

--- src/app/cart-item/cart-item.component.ts
+++ src/app/cart-item/cart-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Item} from "../dto/item";
 
 @Component({
   selector: 'app-cart-item',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart-item.component.scss']
 })
 export class CartItemComponent implements OnInit {
+
+  item = new Item("I001",
+    "src/assets/image/headset.jpg",
+    "Oculus Quest 2 — Advanced All-In-One Virtual Reality Headset — 128 GB",
+    4,
+    99,
+    5);
 
   constructor() { }
 
