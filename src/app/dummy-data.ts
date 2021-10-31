e430461526b5a40d1bde5481257bcaf09f0d0387
Dulanga Nimesha <idulanganimesha@gmail.com>
2021-10-31 11:27:36 +0530

    add dummy data

--- src/app/dummy-data.ts
+++ src/app/dummy-data.ts
@@ -1,2 +1,46 @@
-export class DummyData {
-}
+import {Item} from "./dto/item";
+
+const DUMMY_DATA: Array<Item> = [
+  new Item(
+    "I001",
+    "assets/image/headset.jpg",
+    "Oculus Quest 2 — Advanced All-In-One Virtual Reality Headset — 128 GB",
+    5,
+    99,
+    5),
+  new Item(
+    "I002",
+    "assets/image/headset2.jpg",
+    "Apple AirPods (2nd Generation)",
+    3,
+    25,
+    10),
+  new Item(
+    "I003",
+    "assets/image/nintendo.jpg",
+    "Nintendo Switch - Animal Crossing: New Horizons Edition - Switch",
+    4,
+    45,
+    16),
+  new Item(
+    "I004",
+    "assets/image/adapter.jpg",
+    "Apple 20W USB-C Power Adapter",
+    5,
+    10,
+    30),
+  new Item(
+    "I005",
+    "assets/image/ear_pad.jpg",
+    "Apple EarPods with Lightning Connector - White",
+    4,
+    5,
+    35),
+  new Item(
+    "I006",
+    "assets/image/HDD.jpg",
+    "Seagate Portable 2TB External Hard Drive Portable HDD – USB 3.0 for PC, Mac, PS4, & Xbox",
+    3,
+    16,
+    15)
+];
