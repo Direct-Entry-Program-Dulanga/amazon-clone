b7962c59dfe3023f39be8424cd00ae524d8a7dd4
Dulanga Nimesha <idulanganimesha@gmail.com>
2021-11-03 00:09:39 +0530

    create a service -cart-

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { CartItemComponent } from './cart-item/cart-item.component';
 import {FormsModule} from "@angular/forms";
 import { ItemComponent } from './item/item.component';
 import { HomeComponent } from './home/home.component';
-import {RouterModule, Routes} from "@angular/router";
 import { CartComponent } from './cart/cart.component';
 import {AppRoutingModule} from "./app-routing.module";
 
