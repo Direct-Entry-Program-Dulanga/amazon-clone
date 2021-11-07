f72e978023cc1e0be67d211939fd7eb73f616c19
Dulanga Nimesha <idulanganimesha@gmail.com>
2021-11-08 04:25:07 +0530

    Extract the logic of map function to a seperate function

--- src/app/service/item.service.ts
+++ src/app/service/item.service.ts
@@ -15,38 +15,34 @@ export class ItemService {
   constructor(private http: HttpClient) { }
 
   getAllItems(): Observable<Array<Item>>{
-    return this.http.get<Array<Item>>(this.ITEM_SERVICE_API).pipe(map(items => {
-      const tempItems: Array<Item> = [];
-      items.forEach(i => {
-        const item = i;
-        item.price = (i as any).unitPrice;
-        switch (i.rating as any){
-          case "SECOND":
-            item.rating = 2;
-            break;
-          case "THIRD":
-            item.rating = 3;
-            break;
-          case "FOURTH":
-            item.rating = 4;
-            break;
-          case "FIFTH":
-            item.rating = 5;
-            break;
-          default:
-            item.rating = 1;
-        }
-        item.price = (item as any).unitPrice;
-        tempItems.push(item);
-      })
-      return tempItems;
-    }));
+    return this.http.get<Array<Item>>(this.ITEM_SERVICE_API).pipe(
+
+      map(items =>
+        items.map(convertToItem));
+    }
+
+    getItem(code: string): Observable<Item>{
+      return this.http.get<Item>(`${this.ITEM_SERVICE_API}?code=${code}`);
+    }
   }
 
-  getItem(code: string): Observable<Item>{
-    // const item = DUMMY_DATA.find(item => item.code === code);
-    //
-    // return item? item: null;
-    return this.http.get<Item>(`${this.ITEM_SERVICE_API}?code=${code}`);
+function convertToItem(item: any): Item{
+  switch (item.rating as any){
+    case "SECOND":
+      item.rating = 2;
+      break;
+    case "THIRD":
+      item.rating = 3;
+      break;
+    case "FOURTH":
+      item.rating = 4;
+      break;
+    case "FIFTH":
+      item.rating = 5;
+      break;
+    default:
+      item.rating = 1;
   }
+  item.price = (item as any).unitPrice;
+  return item;
 }
